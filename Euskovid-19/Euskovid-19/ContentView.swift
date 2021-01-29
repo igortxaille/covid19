badf315fed022519b49d1492c64cbfc53ace16c1
igor anton mugertza <igor.anton@ikasle.egibide.org>
2021-01-29 21:07:29 +0100

    proyecto iniciado

---
+++ Euskovid-19/Euskovid-19/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Euskovid-19
+//
+//  Created by  on 29/01/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
